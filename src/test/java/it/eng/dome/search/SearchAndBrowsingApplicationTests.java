67c0c5b5dbe976e717ec2ca082d9b712cfba2327
pasquy <pasquale.vitale@gmail.com>
2024-05-23 11:59:32 +0200

    Add simple test

--- src/test/java/it/eng/dome/search/SearchAndBrowsingApplicationTests.java
+++ src/test/java/it/eng/dome/search/SearchAndBrowsingApplicationTests.java
@@ -1,13 +1,21 @@
 package it.eng.dome.search;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import it.eng.dome.search.service.OfferingProcessor;
 
 @SpringBootTest
 class SearchAndBrowsingApplicationTests {
 
+	@Autowired
+	private OfferingProcessor offeringProcessor;
+
 	@Test
 	void contextLoads() {
+		//assertThat(offeringProcessor).isNotNull();
+		System.out.println( offeringProcessor );
 	}
 
 }
