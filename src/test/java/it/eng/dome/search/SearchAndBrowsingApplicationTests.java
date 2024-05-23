cc0eddd751138368cca7d3cf7635b189f6a6ff6a
pasquy <pasquale.vitale@gmail.com>
2024-05-23 12:20:58 +0200

    add test here

--- src/test/java/it/eng/dome/search/SearchAndBrowsingApplicationTests.java
+++ src/test/java/it/eng/dome/search/SearchAndBrowsingApplicationTests.java
@@ -1,21 +1,15 @@
 package it.eng.dome.search;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import it.eng.dome.search.service.OfferingProcessor;
 
 @SpringBootTest
 class SearchAndBrowsingApplicationTests {
 
-	@Autowired
-	private OfferingProcessor offeringProcessor;
-
 	@Test
-	void contextLoads() {
-		//assertThat(offeringProcessor).isNotNull();
-		System.out.println( offeringProcessor );
+    public void testFindOne() {
+		System.out.println("test here");
+		//https://mkyong.com/spring-boot/spring-boot-spring-data-elasticsearch-example/
 	}
 
 }
